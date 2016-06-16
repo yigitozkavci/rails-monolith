f095e377d96805137b03b369fc519445d2d9c05f
yigitozkavci <yigitozkavci8@gmail.com>
2016-06-16 16:23:00 +0300

    PDFConverterService and Svelte are both being used now. We can compare both.

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,11 +1,32 @@
 class LandingController < ApplicationController
 	def index
+
+		html_document = "Some random html" # This is the html we want to convert.
+
+		########################################################################
+		# Svelte Implementation
+		#
+		# Discovering API endpoints via Svelte.
 	  Svelte::Service.create(url: "http://localhost:10000/api/v1/swagger_doc.json", module_name: 'PDFConverter')
 	  @s = Svelte::Service::PDFConverter::Api::V1::Convert
 
+		# When we have out method calls with already discovered API endpoints, we can make calls.
 		payload = Hash.new
-		payload["html_body"] = "Some random html"
+		payload["html_body"] = html_document
 		pdf = @s.post_api_v1convert(payload).body
-		render json: pdf["pdf_document"]
+		pdf_document = pdf["pdf_document"]
+
+		########################################################################
+		# Stand-alone Service Implementation(service path: app/services/pdf_converter_service.rb)
+		#
+		# Initializing service with the document
+		service = PDFConverterService.new html_document
+
+		# Converting it
+		pdf_document = service.convert_to('pdf')
+		########################################################################
+
+		# Rendering the pdf document
+		render json: pdf_document
 	end
 end
